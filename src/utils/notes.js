85aef0e40da3fe57ae4c7a83898993c67f38a455
Sam <samilalghiffary@gmail.com>
2023-10-24 22:51:58 +0700

    feat: Create dummy notes data

---
+++ src/utils/notes.js
@@ -0,0 +1,46 @@
+const notes = [
+  {
+    title: 'Catatan Pertama',
+    tags: 'penting',
+    body: 'Ini adalah isi dari catatan pertama.',
+    date: '2023-10-24',
+    time: '10:30 AM',
+  },
+  {
+    title: 'Catatan Kedua',
+    tags: 'pekerjaan',
+    body: 'Ini adalah isi dari catatan kedua.',
+    date: '2023-10-25',
+    time: '2:45 PM',
+  },
+  {
+    title: 'Catatan Ketiga',
+    tags: 'belajar',
+    body: 'Ini adalah isi dari catatan ketiga.',
+    date: '2023-10-26',
+    time: '4:15 PM',
+  },
+  {
+    title: 'Catatan Pertama',
+    tags: 'catatan',
+    body: 'Ini adalah isi yang lebih panjang dari catatan pertama. Ini adalah contoh catatan dengan isi yang lebih banyak dan bisa berlanjut ke beberapa paragraf.\n\nMisalnya, di sini kita bisa menambahkan beberapa paragraf lagi untuk membuat catatan ini lebih panjang.\n\nCatatan ini juga bisa berisi informasi yang lebih detail atau apa pun yang Anda butuhkan.',
+    date: '2023-10-24',
+    time: '10:30 AM',
+  },
+  {
+    title: 'Catatan Kedua',
+    tags: 'catatan',
+    body: 'Ini adalah isi yang lebih panjang dari catatan kedua. Anda bisa menambahkan lebih banyak isi seperti yang Anda inginkan. Catatan ini juga bisa berisi daftar, tautan, atau elemen lain sesuai kebutuhan.',
+    date: '2023-10-25',
+    time: '2:45 PM',
+  },
+  {
+    title: 'Catatan Ketiga',
+    tags: 'catatan',
+    body: 'Ini adalah isi yang lebih panjang dari catatan ketiga. Anda bisa terus menambahkan teks ke dalam catatan ini. Ini mungkin berisi informasi yang lebih rinci atau catatan yang panjang.',
+    date: '2023-10-26',
+    time: '4:15 PM',
+  },
+];
+
+export default notes;
