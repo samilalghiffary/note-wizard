85cfbd2ef0077bfa6efa69aca9205158e7f77aa3
Sam <samilalghiffary@gmail.com>
2023-10-27 19:43:18 +0700

    refactor: Change font size at mobile viewport

--- frontend/src/pages/notes/components/EmptyNote.jsx
+++ frontend/src/pages/notes/components/EmptyNote.jsx
@@ -2,8 +2,8 @@ const EmptyNote = ({ heading, paragraph, emptyNoteImage }) => {
   return (
     <div className="w-full h-screen gap-3 flex flex-col justify-center items-center">
       <img src={emptyNoteImage} className="w-60 h-60 mask mask-squircle" alt="writing-wizard" />
-      <h2 className="text-4xl font-semibold text-center">{heading} is empty!</h2>
-      <p className="text-center w-2/4">{paragraph}</p>
+      <h2 className="text-3xl lg:text-4xl font-semibold text-center">{heading} is empty!</h2>
+      <p className="text-center lg:w-2/4">{paragraph}</p>
     </div>
   );
 };
