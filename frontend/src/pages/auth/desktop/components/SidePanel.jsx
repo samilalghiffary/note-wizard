10296c56835045c23acb339990bcc739f80e4eaf
Sam <samilalghiffary@gmail.com>
2023-10-27 19:42:26 +0700

    refactor: Changed background image in CSS to an 'img' element with 'src' attribute

--- frontend/src/pages/auth/desktop/components/SidePanel.jsx
+++ frontend/src/pages/auth/desktop/components/SidePanel.jsx
@@ -1,6 +1,10 @@
+import WizardBook from '@/assets/auth-background.jpeg';
+
 const SidePanel = () => {
   return (
-    <div className="my-background-image basis-7/12 rounded-s-2xl justify-center items-center flex-col flex p-5 bg-secondary-focus"></div>
+    <div className="basis-7/12 rounded-s-2xl justify-center items-center flex-col flex bg-secondary-focus">
+      <img src={WizardBook} className="w-full h-full rounded-s-2xl" alt="" />
+    </div>
   );
 };
 
